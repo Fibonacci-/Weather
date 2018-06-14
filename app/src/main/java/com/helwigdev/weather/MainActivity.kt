91ebfbd7c70426409687b4c3da5eb5af1165d399
Tyler Helwig <helwigdev@gmail.com>
2018-06-14 11:56:54 -0400

    finish initial view inflation from api

--- app/src/main/java/com/helwigdev/weather/MainActivity.kt
+++ app/src/main/java/com/helwigdev/weather/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.helwigdev.weather
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
@@ -43,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun updateWeather(location: String){
+    private fun updateWeather(location: String){
         //should be run inside an asynctask
         //build api url
         val url = "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -60,34 +62,48 @@ class MainActivity : AppCompatActivity() {
         val iconId = weather.getString("icon")
         val weatherString = weather.getString("main")
         val weatherDesc = weather.getString("description")
-
-        //need: temp, humidity, town, country, updateDate
         val tempKelvin = jsonObject.getJSONObject("main").getString("temp").toFloat()
         val humidityPercent = jsonObject.getJSONObject("main").getString("humidity")
         val town = jsonObject.getString("name")
         val country = jsonObject.getJSONObject("sys").getString("country")
         val timestamp = jsonObject.getString("dt").toLong()
+        Log.d("Timestamp",timestamp.toString())
 
         //convert kelvin to fahrenheit
         val tempF = tempKelvin * 1.8 - 459.67
 
         //stamp to date
-        val date = Date(timestamp)
+        val date = Date(timestamp * 1000)
         val sdf = SimpleDateFormat("EEE MMM d, h:mm a", resources.configuration.locale)
         val updatedAt = "At " + sdf.format(date)
 
-        
+        //need to show relevant icon as well
+        val bmUrl = URL("http://openweathermap.org/img/w/$iconId.png")
+        val image = BitmapFactory.decodeStream(bmUrl.openConnection().getInputStream())
+
+
+        runOnUiThread({
+            val formatTempF = "%.2f".format(tempF)
+            tv_weather_main.text = "$weatherString, $formatTempF°F"
+            tv_weather_description.text = weatherDesc
+            tv_humidity_val.text = "$humidityPercent%"
+            tv_location.text = "$town, $country"
+            tv_updated_at.text = updatedAt
+            iv_icon.setImageBitmap(image)
+            unblankView()
+        })
 
     }
 
-    fun blankView(){
+    private fun blankView(){
         for (view in weatherViews){
             view.animate()
                     .alpha(0.0f).duration = 300
         }
     }
 
-    fun unblankView(){
+    private fun unblankView(){
+        pb_current.visibility = View.GONE
         for (view in weatherViews){
             view.animate()
                     .alpha(1.0f).duration = 300
@@ -102,7 +118,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun changeViewToLoading(){
+    private fun changeViewToLoading(){
 
         blankView()
 
